b216844c3756edb7d41cd3d60b0e5dd196f137d3
afetmin <KingPenguins@163.com>
2018-03-03 17:36:39 +0800

    Update fusan.py

--- 釜山行人物关系/fusan.py
+++ 釜山行人物关系/fusan.py
@@ -7,7 +7,9 @@ names = {}
 relationships = {}
 lineNames = []
 """
-使用字典类型names保存人物，该字典的键为人物名称，值为该人物在全文中出现的次数。我使用字典类型relationships保存人物关系的有向边，该字典的键为有向边的起点，值为一个字典edge，edge的键是有向边的终点，值是有向边的权值，代表两个人物之间联系的紧密程度。lineNames是一个缓存变量，保存对每一段分词得到当前段中出现的人物名称，lineName[i]是一个列表，列表中存储第i段中出现过的人物。
+使用字典类型names保存人物，该字典的键为人物名称，值为该人物在全文中出现的次数。使用字典类型relationships保存人物关系的有向边，
+该字典的键为有向边的起点，值为一个字典edge，edge的键是有向边的终点，值是有向边的权值，代表两个人物之间联系的紧密程度。
+lineNames是一个缓存变量，保存对每一段分词得到当前段中出现的人物名称，lineName[i]是一个列表，列表中存储第i段中出现过的人物。
 
 """
 jieba.load_userdict('dict.txt')
@@ -39,7 +41,9 @@ for line in lineNames:
             else:
                 relationships[name1][name2] += 1#两人同时出现则次数加１
 """
-将已经建好的 names 和 relationships 输出到文本，以方便 gephi 可视化处理。输出边的过程中可以过滤可能是冗余的边，这里假设共同出现次数少于 3 次的是冗余边，则在输出时跳过这样的边。输出的节点集合保存为 busan_node.txt ，边集合保存为 busan_edge.node 。
+将已经建好的 names 和 relationships 输出到文本，以方便 gephi 可视化处理。
+输出边的过程中可以过滤可能是冗余的边，这里假设共同出现次数少于 3 次的是冗余边，则在输出时跳过这样的边。
+输出的节点集合保存为 busan_node.txt ，边集合保存为 busan_edge.node 。
 """                
 with codecs.open('fusan_node.txt','w','gbk') as f:
     f.write('Id Label Weight\r\n')

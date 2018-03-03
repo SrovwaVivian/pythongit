74e3db5141feb91889372408f379c34b1cc09f6f
afetmin <KingPenguins@163.com>
2018-03-03 17:50:49 +0800

    Update Singleton.py

--- Singleton.py
+++ Singleton.py
@@ -14,7 +14,7 @@ class MyClass:
 class Singleton(object):
     def __new__(cls, *args, **kw):
         if not hasattr(cls, '_instance'):
-            cls._instance = super(Singleton,cls).__new__(cls, *args, **kw)
+            cls._instance = super(Singleton,cls).__new__(cls)
         return cls._instance
 
 class MyClass2(Singleton):
